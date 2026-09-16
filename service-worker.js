1f1fb59b23ec9ac5764c184094df7bb90e8a127e
BiondaGit <connien@libero.it>
2026-09-16 21:13:12 +0200

    Fix notification display in service worker

--- service-worker.js
+++ service-worker.js
@@ -11,5 +11,7 @@ self.addEventListener('push', event => {
     self.registration.showNotification('Scadenziere', {
       body: 'Push ricevuta dal Service Worker'
     })
+      .then(() => console.log('[SW] NOTIFICA MOSTRATA'))
+      .catch(error => console.error('[SW] ERRORE SHOW NOTIFICATION:', error))
   );
 });
